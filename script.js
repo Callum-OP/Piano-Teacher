20907d45b51061e07ab3164787bb2a2bc4e11334
Callum-OP <callumoprey@gmail.com>
2026-01-27 16:11:16 +0000

    Fixed bug with package checker

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // This is the main script for the piano, buttons and autoplay functionality
 
-// Only show experimental buttons in Development mode
-if (window.env.isPackaged) {
+// Only show experimental buttons in development or web mode
+const isPackaged = window.env ? window.env.isPackaged : false;
+if (isPackaged) {
     document.getElementById('experimental-btn').style.display = 'none';
 }
 
